66ea9c266b8933b8816a0ff2d5262682133733fd
Dante Herrera <dherrera3@my.wctc.edu>
2025-02-25 12:02:46 -0600

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/medals-react-5/",
+  base: "/medals-react-6/",
   plugins: [react()],
 })
